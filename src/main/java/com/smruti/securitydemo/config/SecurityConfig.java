4a6b610af76ea04722d5ca1c935f88bf1355177e
smruti <srnayak165@gmail.com>
2023-01-16 00:23:56 +0530

    authorization feature added

--- src/main/java/com/smruti/securitydemo/config/SecurityConfig.java
+++ src/main/java/com/smruti/securitydemo/config/SecurityConfig.java
@@ -2,11 +2,13 @@ package com.smruti.securitydemo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
@@ -16,7 +18,35 @@ public class SecurityConfig {
     @Bean
     public UserDetailsService userDetails() {
         UserDetails user1 = User.withUsername("smruti").password("{noop}banty").roles("ADMIN").build();
-        UserDetails user2 = User.withUsername("rashmi").password("{noop}little").roles("ADMIN").build();
+        UserDetails user2 = User.withUsername("rashmi").password("{noop}little").roles("STUDENT").build();
         return new InMemoryUserDetailsManager(user1, user2);
     }
+
+    // Authorization
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeHttpRequests(requests -> requests
+                        .requestMatchers("/welcome/**").permitAll()
+                        .requestMatchers("/student").hasRole("ADMIN")
+                        .requestMatchers("/student/{roll}").hasRole("STUDENT")
+                        .anyRequest()
+                        .authenticated())
+                .httpBasic();
+        return http.build();
+    }
+
 }
+
+/* 
+ * http.csrf().disable()
+                .authorizeHttpRequests(requests -> requests
+                        .requestMatchers("/welcome/**").permitAll()
+                        .requestMatchers("/student/**").authenticated())
+ */
+
+ /*
+  * For method level authorize
+  * @PreAuthorize("hasRole('role')") or @PreAuthorize("hasAuthorize('ROLE_role')") 
+  * @EnableMethodSecurity
+  */
---
+++ src/main/java/com/smruti/securitydemo/controller/WelcomeController.java
@@ -0,0 +1,15 @@
+package com.smruti.securitydemo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/welcome")
+public class WelcomeController {
+
+    @GetMapping
+    public String message() {
+        return "Welcome to home page";
+    }
+}
